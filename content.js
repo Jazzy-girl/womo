e1aff8a738718f307d63bed132bf93b0e07119da
Jazzy-girl <rsw5713@g.rit.edu>
2026-08-11 18:27:21 -0400

    pulling and matching from localstorage works

--- content.js
+++ content.js
@@ -3,24 +3,64 @@ const BLOCKED_URLS = [
     "youtube.com",
 ]
 
-const regex = /https:\/\/www[.]reddit[.]com.*/
-const regex2 = RegExp(`https:\/\/www[.]${"reddit"}[.]com.*`)
+
 
 function checkURL(urls){
     const currentURL = window.location.href;
-    console.log(currentURL);
-    for (url in urls){
-        console.log(url);
-    }
-    const matches = currentURL.match(regex2)
-    if (matches != null){
-        console.log("match!");
-        console.log(matches);
-        // MATCH!
-        // document.body.innerHTML = '';
-    }else{
-        console.log("no match!");
-    }
+    browser.storage.local.get("URLS").then((result)=>{
+
+        const inputString = result.URLS;
+        console.log("result.URLS: ");
+        console.log(inputString);
+        const blockedUrls = inputString.split("\n");
+        console.log(`blockedURLS: ${blockedUrls}`);
+        console.log(`length: ${blockedUrls.length}`);
+        
+        
+        for(i = 0; i < blockedUrls.length; i++){
+
+            const url = blockedUrls[i];
+            console.log(`blocked url: ${url}`)
+            const tokens = url.split(".");
+            
+            if (tokens.length == 2){
+            const first = tokens[0];
+            const second = tokens[1];
+            let regex = RegExp(`https:\/\/www\.${first}\.${second}.*`);
+            const matches = currentURL.match(regex);
+            if(matches != null){
+                console.log("match!");
+                console.log(matches);
+                break;
+            }else{
+                console.log("no match");
+            }
+            }else{
+                console.log(`invalid url: ${url}`);
+            }
+
+        }
+
+
+
+
+        
+
+    });
+    
+    // console.log(currentURL);
+    // for (url in urls){
+    //     console.log(url);
+    // }
+    // const matches = currentURL.match(regex)
+    // if (matches != null){
+    //     console.log("match!");
+    //     console.log(matches);
+    //     // MATCH!
+    //     // document.body.innerHTML = '';
+    // }else{
+    //     console.log("no match!");
+    // }
 }
 
 checkURL(BLOCKED_URLS);
